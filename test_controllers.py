6a7d0e2e03f2c9e0c03e80c0684003fcf60b563f
m-c-hill <matthewhill2595@gmail.com>
2022-07-19 11:19:06 +0100

    Routes to show all artists/shows/venues

---
+++ test_controllers.py
@@ -0,0 +1,44 @@
+import pytest
+
+from app import show_venue
+
+# ===========
+#  Fixtures
+# ===========
+
+
+@pytest.fixture
+def show_venue_with_id_1():
+    return {
+        "id": 1,
+        "name": "The Musical Hop",
+        "genres": ["Jazz", "Reggae", "Swing", "Classical", "Folk"],
+        "address": "1015 Folsom Street",
+        "city": "San Francisco",
+        "state": "CA",
+        "phone": "123-123-1234",
+        "website": "https://www.themusicalhop.com",
+        "facebook_link": "https://www.facebook.com/TheMusicalHop",
+        "seeking_talent": True,
+        "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
+        "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+        "past_shows": [
+            {
+                "artist_id": 4,
+                "artist_name": "Guns N Petals",
+                "artist_image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
+                "start_time": "2019-05-21T21:30:00.000Z",
+            }
+        ],
+        "upcoming_shows": [],
+        "past_shows_count": 1,
+        "upcoming_shows_count": 0,
+    }
+
+
+# ===========
+#  Tests
+# ===========
+# def test_show_venue(show_venue_with_id_1):
+#     result = show_venue(venue_id=1)
+#     assert result == show_venue_with_id_1
